544d4d064c8b9addbb21d8e3a0b639e70a4835f0
joaoAugustoParaty <joaodrow@gmail.com>
2023-02-27 13:08:58 -0300

    correções

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,60 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './header.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./header.css";
 
-import logo from '../../assets/images/eco-logo.png'
+import logo from "../../assets/images/eco-logo.png";
+import userIcon from "../../assets/images/user-icon.png";
 
-import { Container, Row } from 'reactstrap';
+import { Container, Row } from "reactstrap";
 
 const Header = () => {
-  return <header className='header'>
-    <Container>
+  return (
+    <header className="header">
+      <Container>
         <Row>
-            <div className="nav__wrapper">
-                <div className="logo">
-                    <img src={logo} alt='logo'/>
-                    <div>
-                        <h1>Directboat</h1>
-                        <p>Desde 1998</p>
-                    </div>
-                </div>
-                </div>
-                <div className="navigation">
-                    <ul className="menu">
-                        <li className="nav__item">
-                           <NavLink to='home'>Home</NavLink>
-                           <NavLink to='home'>Shop</NavLink>
-                           <NavLink to='home'>Cart</NavLink>
-                        </li>
-                    </ul>
-                </div>
-        </Row>
-    </Container>
-  </header>
-}
+          <div className="nav__wrapper">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                <h1>Directboat</h1>
+                <p>Desde 1998</p>
+              </div>
+            </div>
 
-export default Header
+            <div className="navigation">
+              <ul className="menu">
+                <li className="nav__item">
+                  <NavLink to="home">Home</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="home">Shop</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="home">Cart</NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className="nav__icons">
+              <span className="fav__icon">
+                <i class="ri-heart-line"></i>
+              </span>
+              <span className="cart__icon">
+                <i class="ri-shopping-bag-line"></i>
+              </span>
+              <span>
+                <img src={userIcon} alt="" />
+              </span>
+            </div>
+            <div className="mobile__menu">
+              <span>
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
