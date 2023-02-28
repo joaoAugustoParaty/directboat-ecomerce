a35197735df75524e3cff293cafd9018e3bf425c
joaoAugustoParaty <joaodrow@gmail.com>
2023-02-28 15:36:39 -0300

    serviço

---
+++ src/assets/data/serviceData.js
@@ -0,0 +1,29 @@
+const serviceData = [
+    {
+      icon: "ri-truck-line",
+      title: "Entrega grátis",
+      subtitle: "Apartir de R$400,00 em compras a entrega fica por nossa conta. ",
+      subtitle2: "*Promoção valida somente para o minicipio de Paraty!",
+      bg: "#abb0b3",
+    },
+    {
+      icon: "ri-hammer-line",
+      title: "Manutenção e Reparo",
+      subtitle: "Trabalhamos com uma equipe para o reparo e a manutenção do motor da sua embarcação.",
+      bg: "#abb0b3",
+    },
+    {
+      icon: "ri-secure-payment-line",
+      title: "Pagamento seguro",
+      subtitle: "Aceitamos diversos tipos de cartões e pagamento por pix e emitimos a nota fiscal. Faça já o seu orçamento!",
+      bg: "#abb0b3",
+    },
+    {
+      icon: "ri-exchange-dollar-line",
+      title: "Garantimos a devolução",
+      subtitle: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda repellat voluptate.",
+      bg: "#abb0b3",
+    },
+  ];
+  
+  export default serviceData;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,8 @@ import {Container, Row, Col} from "reactstrap";
 
 import heroImg from '../assets/images/motor-logo.png';
 
+import Services from '../services/Services';
+
 
 const Home = () => {
   const year = new Date().getFullYear()
@@ -21,7 +23,7 @@ const Home = () => {
           <Col lg='6' md='6'>
             <div className="hero__content">
               <p className="hero__subtitle">Produto em alta em {year}</p>
-              <h2>Melhore a sua embarcação</h2>
+              <h2>Atualize a sua embarcação</h2>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda repellat voluptate repellendus, odit ducimus quas, voluptatum blanditiis nulla vero esse ratione aliquid ea reprehenderit! Obcaecati voluptatem excepturi dignissimos dolorem delectus!</p>
 
               <motion.button whileTap={{scale:1.2}} className="buy__btn"><Link to="/shop">Compre agora</Link></motion.button>
@@ -36,6 +38,7 @@ const Home = () => {
       </Container>
 
     </section>
+    <Services/>
   </Helmet>
 }
 
