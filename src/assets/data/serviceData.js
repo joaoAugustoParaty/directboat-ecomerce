c9e44be38304923583dc913f266f80c557c5c7e4
joaoAugustoParaty <joaodrow@gmail.com>
2023-02-28 23:16:13 -0300

    correção das cores

--- src/assets/data/serviceData.js
+++ src/assets/data/serviceData.js
@@ -4,25 +4,25 @@ const serviceData = [
       title: "Entrega grátis",
       subtitle: "Apartir de R$400,00 em compras a entrega fica por nossa conta. ",
       subtitle2: "*Promoção valida somente para o minicipio de Paraty!",
-      bg: "#abb0b3",
+      bg: "#fdefe6",
     },
     {
-      icon: "ri-hammer-line",
+      icon: "ri-tools-line",
       title: "Manutenção e Reparo",
       subtitle: "Trabalhamos com uma equipe para o reparo e a manutenção do motor da sua embarcação.",
-      bg: "#abb0b3",
+      bg: "#ceebe9",
     },
     {
       icon: "ri-secure-payment-line",
       title: "Pagamento seguro",
       subtitle: "Aceitamos diversos tipos de cartões e pagamento por pix e emitimos a nota fiscal. Faça já o seu orçamento!",
-      bg: "#abb0b3",
+      bg: "#e2f2b2",
     },
     {
       icon: "ri-exchange-dollar-line",
       title: "Garantimos a devolução",
       subtitle: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda repellat voluptate.",
-      bg: "#abb0b3",
+      bg: "#d6e5fb",
     },
   ];
   
